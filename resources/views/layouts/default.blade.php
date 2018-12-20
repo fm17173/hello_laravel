1e53ea68ed7f6b701d5cf9cef83721c18cf323c1
vagrant <vagrant@homestead>
2018-12-20 13:35:48 +0000

    初始化样式

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -2,8 +2,18 @@
 <head>
     <meta charset="UTF-8">
     <title>@yield('title', 'Weibo App') - Laravel 新手入门教程</title>
+    <link rel="stylesheet" href="{{mix('css/app.css')}}">
 </head>
 <body>
+<nav class="navbar navbar-expand navbar-dark bg-dark">
+    <div class="container">
+        <a href="/" class="navbar-brand">Weibo App</a>
+        <ul class="navbar-nav justify-content-end">
+            <li class="nav-item"><a href="/help" class="nav-link">帮助</a></li>
+            <li class="nav-item"><a href="#" class="nav-link">登录</a></li>
+        </ul>
+    </div>
+</nav>
 @yield('content')
 </body>
 </html>
